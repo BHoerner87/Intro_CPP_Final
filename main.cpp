0a4b27fa37ab3a6aa7b9441750bd235025433967
BrianLinux <brianhoerner87@gmail.com>
2020-11-30 17:37:02 -0500

    Initial Commit for this semester's final project.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Brian Hoerner
+// 30 Novemember 2020
+// Event Planner
+// This is an event planning software that operates as a standard calendar.
+// Users can add, remove, and view events.
+
+#include <iostream>
+#include <iomanip>
+#include <string>
