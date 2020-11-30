46c59265ae7f8b6ae603e0b134d6b47f561f8ec3
BrianLinux <brianhoerner87@gmail.com>
2020-11-30 17:44:00 -0500

    Added initial functions in comments. Probably more to come.

--- main.cpp
+++ main.cpp
@@ -7,3 +7,30 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+
+using namespace std;
+
+int main()
+{
+    // Main Menu
+    // 1. Add an Event
+    // 2. Remove an Event
+    // 3. View Events
+    // 4. Quit
+
+    cout << "----------------------------------------|----------------------------------------";
+}
+
+/* void functionAdd()
+*/
+
+/* void functionRemove()
+    In which month?
+        List events for month
+        Which event? 1., 2., 3...
+*/
+
+/* void functionDisplay
+    Display All
+    Display by Month
+    Display by day (still have to pick a month or maybe take two cins?)
